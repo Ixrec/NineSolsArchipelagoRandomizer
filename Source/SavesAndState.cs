9a06b76341e72d7f0d5db70e2e621a4145b2e422
Ixrec <ericrhitchcock@gmail.com>
2025-01-24 01:22:58 +0000

    make FSP node auto-open itself; not mechanically necessary, but less likely to confuse the player since it looks dead without this

--- Source/SavesAndState.cs
+++ Source/SavesAndState.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
+using System.Reflection;
 
 namespace ArchipelagoRandomizer;
 
@@ -29,7 +31,9 @@ internal class SavesAndState {
         // This magic string came from evaluating SingletonBehaviour<SaveManager>.Instance.currentPlayerData.lastTeleportPointPath in the UE console.
         var fspTeleportPointPath = "9115d3446fcc24abab2c0030d55abd1eTeleportPointData";
         var teleportPointData = SingletonBehaviour<GameFlagManager>.Instance.GetTeleportPointWithPath(fspTeleportPointPath);
+        Log.Info($"StartMenuLogic_NewGameChangeScene_APImpl calling StartGameGoTo");
         await SingletonBehaviour<ApplicationCore>.Instance.StartGameGoTo(teleportPointData);
+        Log.Info($"StartMenuLogic_NewGameChangeScene_APImpl called StartGameGoTo");
 
         // and edit whatever state flags the randomizer needs to be different from vanilla.
 
@@ -47,6 +51,54 @@ internal class SavesAndState {
         hasFusangHornFlag.PlayerPicked();
         var hasTeleportFlag = (PlayerAbilityData)SingletonBehaviour<SaveManager>.Instance.allFlags.FlagDict["950f8f3273611424d9b42ab209e8cac8PlayerAbilityData"];
         hasTeleportFlag.PlayerPicked();
+
+        Log.Info($"StartMenuLogic_NewGameChangeScene_APImpl edited flags");
+
+        SingletonBehaviour<GameCore>.Instance.gameLevel.OnLevelStartPlaying.AddListener(FSPLevelStartPlayingHandler);
+    }
+
+    // All of this is for making the FSP save point do just its opening animation the first time we load the FSP level without breaking anything
+    private static bool automatedFirstFSPSavePointOpening = false;
+    static void FSPLevelStartPlayingHandler() {
+        Log.Info($"FSPLevelStartPlayingHandler now trying to open save point");
+        var spgo = GameObject.Find("AG_S2/Room/議會古樹管理 FSM Variant/FSM Animator/LogicRoot/SavePoint_AG_S2");
+
+        // This "isActived" flag is how the save point knows to do a smaller "SP_PlayerEnter" animation next time,
+        // without visually reverting to its unopened state.
+        var isActived = typeof(SavePoint).GetField("isActived", BindingFlags.Instance | BindingFlags.NonPublic);
+        isActived.SetValue(spgo.GetComponent<SavePoint>(), true);
+
+        automatedFirstFSPSavePointOpening = true;
+        var spa = spgo.transform.Find("LotusSP_AG_S2").GetComponent<Animator>();
+        spa.Play("SP_opening");
+
+        SingletonBehaviour<GameCore>.Instance.gameLevel.OnLevelStartPlaying.RemoveListener(FSPLevelStartPlayingHandler);
+    }
+    [HarmonyPrefix, HarmonyPatch(typeof(AnimationEvents), "InvokeAnimationEvent")]
+    static bool AnimationEvents_InvokeAnimationEvent(AnimationEvents __instance, AnimationEvents.AnimationEvent e) {
+        if (!automatedFirstFSPSavePointOpening)
+            return true;
+
+        var goPath = LocationTriggers.GetFullDisambiguatedPath(__instance.gameObject);
+        if (goPath == "AG_S2/Room/議會古樹管理 FSM Variant/FSM Animator/LogicRoot/SavePoint_AG_S2/LotusSP_AG_S2") {
+            Log.Info($"AnimationEvents_InvokeAnimationEvent skipping normal events for FSP node activation and doing playerexit anim instead");
+            automatedFirstFSPSavePointOpening = false;
+            var spa = __instance.gameObject.GetComponent<Animator>();
+            spa.Play("SP_PlayerExit");
+            return false;
+        }
+        return true;
+    }
+    [HarmonyPrefix, HarmonyPatch(typeof(InteractInteraction), "InteractedImplementation")]
+    static void InteractInteraction_InteractedImplementation(InteractInteraction __instance) {
+        if (!automatedFirstFSPSavePointOpening)
+            return;
+
+        var goPath = LocationTriggers.GetFullDisambiguatedPath(__instance.gameObject);
+        if (goPath == "AG_S2/Room/議會古樹管理 FSM Variant/FSM Animator/LogicRoot/SavePoint_AG_S2/Interactable_Interact/Interact Interaction") {
+            Log.Info($"InteractInteraction_InteractedImplementation cancelling the upcoming animation event skip because the player actually interacted with the node");
+            automatedFirstFSPSavePointOpening = false;
+        }
     }
 
     // This is the best approach I've found so far for editing FSMs: intercept transitions into a state and then do a manual transition afterward.
@@ -63,10 +115,10 @@ internal class SavesAndState {
         }
 
         if (__instance.name == "[State] 第一次爆走") {
+            // Not that we'll ever enter this state in practice after giving ourselves the Fusang Horn...
             var goPath = LocationTriggers.GetFullDisambiguatedPath(__instance.gameObject);
             if (goPath == "AG_S2/Room/議會古樹管理 FSM Variant/--[States]/FSM/[State] 第一次爆走") {
                 Log.Info($"GeneralState_OnStateEnter forcing FSP root node to its default state despite the base game wanting to do the 1st Lear talk");
-                // TODO: why does this make the node look dead until our first time interacting with it???
                 var fspRootNodeNormalState = GameObject.Find("AG_S2/Room/議會古樹管理 FSM Variant/--[States]/FSM/[State] 普通存檔點");
                 fspRootNodeNormalState?.GetComponent<GeneralState>()?.OnStateEnter();
             }
