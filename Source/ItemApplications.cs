243e912d7dd563e728121fc42876a0ab393ed279
Ixrec <ericrhitchcock@gmail.com>
2025-01-26 13:33:16 +0000

    reimplement Pipe Vials, since they follow the same pattern as CUs

--- Source/ItemApplications.cs
+++ Source/ItemApplications.cs
@@ -156,7 +156,7 @@ internal class ItemApplications {
             case Item.DarkSteel: inventoryItem = inventory[2].rawCollection[13]; break;
             case Item.HerbCatalyst: inventoryItem = inventory[2].rawCollection[14]; break;
 
-            // inventory[3]
+            // inventory[3] - all of these either aren't randomized items, or are handled elsewhere
             //Azure Sand
             //Jin
             //Root Stabilizer ???
@@ -167,7 +167,7 @@ internal class ItemApplications {
             //Azure Bow
             //Azure Sand Magazine
             //Medicine Pipe
-            case Item.PipeVial: inventoryItem = inventory[3].rawCollection[10]; break;
+            //Pipe Vial
             default: break;
         }
         if (inventoryItem != null) {
@@ -300,6 +300,9 @@ internal class ItemApplications {
         if (item == Item.ComputingUnit) {
             return ApplyComputingUnit(count);
         }
+        if (item == Item.PipeVial) {
+            return ApplyPipeVial(count);
+        }
 
         ToastManager.Toast($"unable to apply item {item} (count = {count})");
         return (null, false);
@@ -366,6 +369,67 @@ internal class ItemApplications {
         //Log.Info($"ApplyComputingUnit unshuffledCUCount={unshuffledCUCount}, inGameInventoryCount={inGameInventoryCount}");
 
         var cuInventoryItem = SingletonBehaviour<UIManager>.Instance.allItemCollections[3].rawCollection[6];
+        cuInventoryItem.unlocked.CurrentValue = (inGameInventoryCount > 0);
+        cuInventoryItem.acquired.CurrentValue = (inGameInventoryCount > 0);
+        (cuInventoryItem as ItemData)!.ownNum.CurrentValue = inGameInventoryCount;
+
+        return (cuInventoryItem, true);
+    }
+
+    /*
+     * Pipe Vials follow the same pattern as Computing Units.
+     */
+
+    private static string[] shopPVs = [
+        "5c864e36f2cef9d4f8c24c0aa84010bePlayerAbilityData", // (升級) 煙斗使用次數_Lv1_議會販賣機
+        "3f6040257515a41499d36e910a4a6e79PlayerAbilityData", // (升級) 煙斗使用次數_Lv2_議會販賣機
+        "ec1fe3b64944cd643b2020f35e82f023PlayerAbilityData", // (升級) 煙斗使用次數_Lv3_議會販賣機
+        "dd5adfbe0ac50fe4795b796153ee646dPlayerAbilityData", // (升級) 煙斗使用次數_Lv4_議會販賣機
+    ];
+    private static string[] otherPVs = [
+        "ce7166af4ef39d7468c4ccc464fd90b9PlayerAbilityData", // (升級) 煙斗使用次數_AG_SG1
+        "41c960dfcaaf7a14f813342db16f0481PlayerAbilityData", // (升級) 煙斗使用次數_A3SG4日昇樓內 (= Inside Daybreak Tower)
+    ];
+
+    // TODO: move the similar parts of CU and PV methods into a shared helper? maybe after we're sure there aren't more weird items
+    private static (GameFlagDescriptable?, bool) ApplyPipeVial(int apCount) {
+        Log.Info($"ApplyPipeVial(apCount={apCount})");
+        var unshuffledPVs = shopPVs;
+        var shuffledPVs = otherPVs;
+
+        var maxAPPVs = shuffledPVs.Length;
+        if (apCount < 0 || apCount > maxAPPVs) {
+            Log.Error($"ApplyPipeVial passed {apCount}, but apCount must be between 0 and (on this slot) {maxAPPVs}");
+            return (null, false);
+        }
+
+        var flagDict = SingletonBehaviour<SaveManager>.Instance.allFlags.FlagDict;
+
+        for (var i = 0; i < shuffledPVs.Length; i++) {
+            var flagId = shuffledPVs[i];
+            var pad = flagDict[flagId] as PlayerAbilityData;
+
+            //Log.Info($"ApplyPipeVial setting {i}-th (shuffled by AP) PAD to {(i < apCount)}");
+            pad.unlocked.CurrentValue = (i < apCount);
+            pad.acquired.CurrentValue = (i < apCount);
+            if (i < apCount) {
+                padActivate.Invoke(pad, []);
+            } else {
+                padDeactivate.Invoke(pad, []);
+            }
+        }
+
+        /*foreach (var id in unshuffledPVs) {
+            var pad = flagDict[id] as PlayerAbilityData;
+            Log.Info($"ApplyPipeVial unshuffledPV {id} / {pad.name} is {pad.acquired.CurrentValue}");
+        }*/
+
+        var unshuffledPVCount = unshuffledPVs.Sum(flagId =>
+            (flagDict[flagId] as PlayerAbilityData)!.acquired.CurrentValue ? 1 : 0);
+        var inGameInventoryCount = unshuffledPVCount + apCount;
+        //Log.Info($"ApplyPipeVial unshuffledPVCount={unshuffledPVCount}, inGameInventoryCount={inGameInventoryCount}");
+
+        var cuInventoryItem = SingletonBehaviour<UIManager>.Instance.allItemCollections[3].rawCollection[10];
         cuInventoryItem.unlocked.CurrentValue = (inGameInventoryCount > 0);
         cuInventoryItem.acquired.CurrentValue = (inGameInventoryCount > 0);
         (cuInventoryItem as ItemData)!.ownNum.CurrentValue = inGameInventoryCount;
