95922b6f7c494259297188bebd22e8f37506f236
Ixrec <ericrhitchcock@gmail.com>
2025-03-02 16:53:07 +0000

    split up StateTransitions into smaller files, now that it's become clear putting all state transitions in one file is unhelpful

---
+++ Source/ScriptedEventEdits/JiAtDaybreakTower.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ArchipelagoRandomizer;
+
+[HarmonyPatch]
+internal class JiAtDaybreakTower {
+    /*
+     * Make Ji always appear at Daybreak Tower to give you the music sheet
+     */
+    [HarmonyPostfix, HarmonyPatch(typeof(GeneralState), "OnStateEnter")]
+    private static void GeneralState_OnStateEnter(GeneralState __instance) {
+        // despite the "On" name, this state represents Ji *not* being at the Daybreak Tower
+        if (__instance.name == "[State] On") {
+            var goPath = LocationTriggers.GetFullDisambiguatedPath(__instance.gameObject);
+            if (goPath == "A3_S1/Room/Prefab/Gameplay_BellTower/General FSM Object_On And Off Switch Variant/--[States]/FSM/[State] On") {
+                Log.Info($"GeneralState_OnStateEnter forcing Ji to exist at Daybreak Tower despite the base game hiding him");
+                var jiDoesExistAtDaybreakTowerState = GameObject.Find("A3_S1/Room/Prefab/Gameplay_BellTower/General FSM Object_On And Off Switch Variant/--[States]/FSM/[State] Off");
+                jiDoesExistAtDaybreakTowerState?.GetComponent<GeneralState>()?.OnStateEnter();
+            }
+        }
+    }
+}
--- Source/ScriptedEventEdits/LimitlessRealm.cs
+++ Source/ScriptedEventEdits/LimitlessRealm.cs
@@ -1,11 +1,12 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace ArchipelagoRandomizer;
 
 [HarmonyPatch]
-class LimitlessRealm
-{
-    // For now, all we want to do with Limitless Realm segments is skip them.
+class LimitlessRealm {
+    // For now, all we want to do with Limitless Realm tutorial segments is skip them.
+    // Note that this patch does not affect the FSP's Limitless Realm segments; those use a state machine we handle separately below.
     [HarmonyPrefix, HarmonyPatch(typeof(SavePoint), nameof(SavePoint.CurrentTutorial))]
     public static bool SavePoint_CurrentTutorial(SavePoint __instance, ref TutorialEntry? __result) {
         // The vanilla behavior is basically to return .berserkBindingTutorial, whether or not it's null
@@ -27,4 +28,38 @@ class LimitlessRealm
      * A TutorialEntry object is basically a SceneConnectionPoint with some validity checks (usually if the player has an ability yet),
      * since the actual "tutorial" is always its own scene.
      */
+
+    /*
+     * Make the FSP root node always appear in its default state, so it won't send you into any of
+     * the usual Lear cutscenes (which are redundant because we start rando with full teleport).
+     */
+    [HarmonyPostfix, HarmonyPatch(typeof(GeneralState), "OnStateEnter")]
+    private static void GeneralState_OnStateEnter(GeneralState __instance) {
+        // The 1st and 2nd states probably would never be hit anyway since we give ourselves horn + teleport
+        // on new game creation in rando, but might as well play it safe.
+        if (__instance.name == "[State] 第一次爆走") {
+            var goPath = LocationTriggers.GetFullDisambiguatedPath(__instance.gameObject);
+            if (goPath == "AG_S2/Room/議會古樹管理 FSM Variant/--[States]/FSM/[State] 第一次爆走") {
+                Log.Info($"GeneralState_OnStateEnter forcing FSP root node to its default state despite the base game wanting to do the 1st Lear talk");
+                var fspRootNodeNormalState = GameObject.Find("AG_S2/Room/議會古樹管理 FSM Variant/--[States]/FSM/[State] 普通存檔點");
+                fspRootNodeNormalState?.GetComponent<GeneralState>()?.OnStateEnter();
+            }
+        }
+        if (__instance.name == "[State] 第二次爆走") {
+            var goPath = LocationTriggers.GetFullDisambiguatedPath(__instance.gameObject);
+            if (goPath == "AG_S2/Room/議會古樹管理 FSM Variant/--[States]/FSM/[State] 第二次爆走") {
+                Log.Info($"GeneralState_OnStateEnter forcing FSP root node to its default state despite the base game wanting to do the 2nd Lear talk");
+                var fspRootNodeNormalState = GameObject.Find("AG_S2/Room/議會古樹管理 FSM Variant/--[States]/FSM/[State] 普通存檔點");
+                fspRootNodeNormalState?.GetComponent<GeneralState>()?.OnStateEnter();
+            }
+        }
+        if (__instance.name == "[State] 第三次爆走") {
+            var goPath = LocationTriggers.GetFullDisambiguatedPath(__instance.gameObject);
+            if (goPath == "AG_S2/Room/議會古樹管理 FSM Variant/--[States]/FSM/[State] 第三次爆走") {
+                Log.Info($"GeneralState_OnStateEnter forcing FSP root node to its default state despite the base game wanting to do the 3rd Lear talk");
+                var fspRootNodeNormalState = GameObject.Find("AG_S2/Room/議會古樹管理 FSM Variant/--[States]/FSM/[State] 普通存檔點");
+                fspRootNodeNormalState?.GetComponent<GeneralState>()?.OnStateEnter();
+            }
+        }
+    }
 }
