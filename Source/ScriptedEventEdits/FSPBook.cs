e53f9cd3c4700a0daa54b911d895f8cf48b3fdf4
Ixrec <ericrhitchcock@gmail.com>
2025-03-02 15:34:42 +0000

    prevent Shuanshuan's book location from softlocking itself if you get the book item first

---
+++ Source/ScriptedEventEdits/FSPBook.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArchipelagoRandomizer.ScriptedEventEdits;
+
+// Shuanshuan's book will not spawn on the FSP bookshelf if you:
+// - already have the item in your inventory
+// - already gave the book to Chiyou
+// but we want these items/locations to be doable in any order, so these conditions need bypassing.
+
+[HarmonyPatch]
+class FSPBook
+{
+    [HarmonyPostfix, HarmonyPatch(typeof(AbstractConditionComp), "FinalResult", MethodType.Getter)]
+    static void AbstractConditionComp_get_FinalResult(AbstractConditionComp __instance, ref bool __result) {
+        if (
+            __instance.name == "[Condition] 還沒拿過小說" ||
+            __instance.name == "[Condition] 蚩尤還沒要小說"
+        ) {
+            var locationsChecked = APSaveManager.CurrentAPSaveData?.locationsChecked;
+            var locName = Location.FSP_SHUANSHUAN_BOOK.ToString();
+            if (locationsChecked != null && locationsChecked.ContainsKey(locName) && locationsChecked[locName])
+                // if we've already checked the Book location, no need to force the book to appear again
+                return;
+
+            if (__result == true)
+                // if this condition already evaluated to true, then we don't need to change its result to make the book appear
+                return;
+
+            var goPath = LocationTriggers.GetFullDisambiguatedPath(__instance.gameObject);
+            if (goPath == "AG_S2/Room/NPCs/議會演出相關Binding/軒軒小說 FSM/--[States]/FSM/[State] Init/[Action] 小說在 Transition/[Condition] 還沒拿過小說") {
+                Log.Info($"AbstractConditionComp_get_FinalResult forcing the 'you don't already have Shuanshuan's book' check to pass so that the FSP_SHUANSHUAN_BOOK location is checkable");
+                __result = true;
+            }
+            if (goPath == "AG_S2/Room/NPCs/議會演出相關Binding/軒軒小說 FSM/--[States]/FSM/[State] Init/[Action] 小說在 Transition/[Condition] 蚩尤還沒要小說") {
+                Log.Info($"AbstractConditionComp_get_FinalResult forcing the 'you haven't already given Shuanshuan's book to Chiyou' check to pass so that the FSP_SHUANSHUAN_BOOK location is checkable");
+                __result = true;
+            }
+            return;
+        }
+    }
+}
