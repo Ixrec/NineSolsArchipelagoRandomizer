33464bcb91c8533a6d02ba623934b8e4a911434b
Ixrec <ericrhitchcock@gmail.com>
2025-09-25 01:50:52 +0100

    show SMB inventory item when SMB is acquired

--- Source/ItemsAndLocations/ItemApplications.cs
+++ Source/ItemsAndLocations/ItemApplications.cs
@@ -190,15 +190,24 @@ internal class ItemApplications {
 
         // we're unlikely to use these, but: RollDodgeAbility is regular ground dash
         PlayerAbilityData? ability = null;
+        ItemData? abilityInventoryItem = null;
         switch (item) {
             case Item.TaiChiKick: ability = Player.i.mainAbilities.ParryJumpKickAbility; break;
             case Item.AirDash: ability = Player.i.mainAbilities.RollDodgeInAirUpgrade; break;
             case Item.ChargedStrike: ability = Player.i.mainAbilities.ChargedAttackAbility; break;
             case Item.CloudLeap: ability = Player.i.mainAbilities.AirJumpAbility; break;
-            case Item.SuperMutantBuster: ability = Player.i.mainAbilities.KillZombieFooAbility; break;
+            case Item.SuperMutantBuster:
+                ability = Player.i.mainAbilities.KillZombieFooAbility;
+                abilityInventoryItem = (ItemData)SingletonBehaviour<SaveManager>.Instance.allFlags.FlagDict["9d0a49e76ce762c47844dbd20e21c737ItemData"];
+                break;
             case Item.UnboundedCounter: ability = Player.i.mainAbilities.ParryCounterAbility; break;
             case Item.MysticNymphScoutMode: ability = Player.i.mainAbilities.HackDroneAbility; break;
             default: break;
+        }
+        if (abilityInventoryItem != null) {
+            abilityInventoryItem.acquired.SetCurrentValue(count > 0);
+            abilityInventoryItem.unlocked.SetCurrentValue(count > 0);
+            abilityInventoryItem.ownNum.SetCurrentValue(count);
         }
         if (ability != null) {
             ability.acquired.SetCurrentValue(count > 0);
