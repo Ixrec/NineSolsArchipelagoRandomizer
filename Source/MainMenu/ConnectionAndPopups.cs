d1ca05d72771a18ebcf892f3427d35a17af76e56
Ixrec <ericrhitchcock@gmail.com>
2025-10-08 06:34:22 +0100

    TODO comment about version checks

--- Source/MainMenu/ConnectionAndPopups.cs
+++ Source/MainMenu/ConnectionAndPopups.cs
@@ -260,6 +260,7 @@ internal class ConnectionAndPopups {
         }
         if (SlotData != null && SlotData.ContainsKey("apworld_version")) {
             var worldVersion = Version.Parse((string)SlotData["apworld_version"]);
+            // TODO: do we want to warn if world version > mod version? can we get mod version in here without risking it going stale?
             RemovedAbilities.ApplyWorldVersion(worldVersion);
         }
 
