fdc7cd5610dc1e4dbe1c24310796234b0697590e
Ixrec <ericrhitchcock@gmail.com>
2025-12-12 06:34:09 +0000

    add more exceptions to the shop block so it's less likely to annoy players

--- Source/Features/Shops.cs
+++ Source/Features/Shops.cs
@@ -1,5 +1,4 @@
 ﻿using HarmonyLib;
-using NineSolsAPI;
 
 namespace ArchipelagoRandomizer;
 
@@ -12,25 +11,37 @@ internal class Shops {
         LogicDifficulty = logicDifficulty ?? 0;
     }
 
-    // we only want to force the player to spend their first Dark Steel on CPS if they're on medium or higher logic,
-    // which means only block purchase of Dark Steel-requiring shop slots that aren't CPS itself, until CPS is acquired
+    // we only want to force the player to spend Dark Steel on CPS if they're at risk of "missing" a logically required CPS barrier skip
     private static bool ShouldBlock(MerchandiseData __instance) {
+        // vanilla logic doesn't expect bow tricks
         if (LogicDifficulty == 0)
             return false;
 
+        // if this shop slot doesn't require a Dark Steel, then it's not relevant
         var entries = __instance.requireMaterialEntriesToBuy;
         if (entries.Count != 1)
             return false;
-
         var entry = entries[0];
         if (entry?.item.Title != "Dark Steel")
             return false;
 
+        // CPS is only logically relevant as a substitute for Charged Strike, so if the player already has CS then we don't care about blocking shop items any more
+        if (Player.i.mainAbilities.ChargedAttackAbility.IsAcquired)
+            return false;
+
+        // don't block CPS itself from being purchased, since the whole point is to prevent the player from "missing" CPS
         if (__instance.name == "Merchandise_2_1_貫穿箭LV2")
             return false;
 
+        // if CPS already has been purchased, then we don't care about blocking shop items any more
         var cloudPiercerS = (PlayerWeaponData)SingletonBehaviour<SaveManager>.Instance.allFlags.FlagDict["2f7009a00edd57c4fa4332ffcd15396aPlayerWeaponData"];
         if (cloudPiercerS.IsAcquired)
+            return false;
+
+        // if the player has multiple unspent Dark Steels, then it's safe to let them buy a non-CPS upgrade before CPS
+        var darkSteelInventoryItem = SingletonBehaviour<UIManager>.Instance.allItemCollections[2].rawCollection[13];
+        var darkSteelCount = ((ItemData)darkSteelInventoryItem).ownNum.CurrentValue;
+        if (darkSteelCount > 1)
             return false;
 
         return true;
@@ -49,8 +60,10 @@ internal class Shops {
         if (ShouldBlock(__instance)) {
             __result = $"{LoadingScreenTips.apRainbow}: " +
                 "\n" +
-                "<color=orange>You must purchase Cloud Piercer S first</color>, " +
-                "because you're playing on medium or higher logic difficulty." +
+                "Because you're playing on medium or higher logic difficulty, " +
+                "you don't have Charged Strike yet, " +
+                "and you only have one Dark Steel available right now, " +
+                "<color=orange>you must spend your last Dark Steel on Cloud Piercer S</color>. " +
                 "\n\n" +
                 __result;
         }
