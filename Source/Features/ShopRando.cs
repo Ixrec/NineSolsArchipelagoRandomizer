882533f10d9f27ac877cd73c9910207edf1ef139
Ixrec <ericrhitchcock@gmail.com>
2026-04-09 00:22:37 +0100

    auto-hint prog and useful shop items

--- Source/Features/ShopRando.cs
+++ Source/Features/ShopRando.cs
@@ -403,14 +403,46 @@ internal class ShopRando {
                 APSaveManager.CurrentAPSaveData.otherPersistentModFlags.TryGetValue(ShopUnlocks.KuafuExtraInventory_ModSaveFlag, out var kuafuExtraInventoryUnlocked) &&
                 kuafuExtraInventoryUnlocked
             ) {
+                EnsureShopLocationAutoHinted(location);
                 __result = true;
             } else {
                 __result = false;
             }
         } else {
             // all other randomized shop slots should simply always be visible
+            EnsureShopLocationAutoHinted(location);
             __result = true;
         }
+    }
+
+    private static void EnsureShopLocationAutoHinted(Location location) {
+        if (APSaveManager.CurrentAPSaveData == null)
+            return;
+        if (ConnectionAndPopups.APSession == null)
+            return;
+        if (!LocationNames.locationToArchipelagoId.ContainsKey(location))
+            return;
+        if (APSaveManager.CurrentAPSaveData.scoutedLocations == null)
+            return;
+        if (!APSaveManager.CurrentAPSaveData.scoutedLocations.TryGetValue(location, out var scoutedItemInfo))
+            return;
+
+        // For now, we auto-hint prog and useful items
+        if (!scoutedItemInfo.Flags.HasFlag(Archipelago.MultiClient.Net.Enums.ItemFlags.Advancement) &&
+            !scoutedItemInfo.Flags.HasFlag(Archipelago.MultiClient.Net.Enums.ItemFlags.NeverExclude))
+            return;
+
+        // Use save data flags to avoid wasting network requests on a hint we've already created
+        var flagForLocation = location.ToString() + "_HasBeenHinted";
+        var modFlags = APSaveManager.CurrentAPSaveData.otherPersistentModFlags;
+        if (modFlags.ContainsKey(flagForLocation) && modFlags[flagForLocation])
+            return;
+
+        var locationId = LocationNames.locationToArchipelagoId[location];
+        Log.Info($"Creating AP hint for shop location {location} / {locationId} because it contains a progression and/or useful item");
+        ConnectionAndPopups.APSession.Hints.CreateHints(locationIds: [locationId]);
+
+        modFlags[flagForLocation] = true;
     }
 
     [HarmonyPrefix, HarmonyPatch(typeof(MerchandiseData), "CanTrade", MethodType.Getter)]
